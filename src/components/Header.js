6c832d657d08471d79f1f054cfd771427fcf0211
unknown <leonXperniciaro@gmail.com>
2023-08-08 13:35:29 -0400

    logo png

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Menu from './Menu';
 import Hamburger from './Hamburger';
-import logo from '../images/logo.svg';
+import logo from '../images/logo.png';
 import logoMobile from '../images/logo-mobile.svg';
 import MenuMobile from './MenuMobile';
 
